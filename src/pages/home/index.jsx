5ac23e545a6addf86fc7cf1a065a3a39199881b7
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2023-07-26 13:17:39 +0530

    Added debouncing

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,8 @@
-import { useSelector, useDispatch } from 'react-redux';
-import Header from '../../components/Header';
-import { logOutAction } from '../../store/auth/action';
+import { useSelector, useDispatch } from "react-redux";
+import Header from "../../components/Header";
+import { logOutAction } from "../../store/auth/action";
+import DebounceComponent from "../../components/DebounceComponent";
+import ThrottleComponent from "../../components/ThrottleComponent";
 
 const Home = () => {
   const user = useSelector((state) => state?.auth?.user);
@@ -10,7 +12,15 @@ const Home = () => {
   return (
     <>
       <Header user={user} onLogout={() => dispatch(logOutAction())} />
-      <h2 className='text-center text-xl mt-32'>Welcome to the home page!</h2>
+      <div className="max-w-sm border rounded my-5 mx-auto">
+        <h2 className="text-center text-xl p-5">Debouncing</h2>
+        <DebounceComponent />
+      </div>
+
+      <div className="max-w-sm border rounded my-5 mx-auto">
+        <h2 className="text-center text-xl p-5">Throttling</h2>
+        <ThrottleComponent />
+      </div>
     </>
   );
 };
